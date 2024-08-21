87d1cd955d3fc1a9d398e8dd1329eaaa1fdb7712
Maya Sagalin <mayasag10@gmail.com>
2024-08-21 19:56:04 +0300

    added footer to pages

--- client/src/pages/CalendarViewPage.js
+++ client/src/pages/CalendarViewPage.js
@@ -1,5 +1,14 @@
-const CalendarViewPage = () =>(
-    <h1>Calendar view</h1>
-);
+import Footer from "../component/Footer";
+import NavBar from "../component/NavBar";
 
+const CalendarViewPage = () =>{
+    return(
+        <>
+            <h1>Calendar view</h1>
+            <NavBar/>
+            <Footer/>
+        </>
+        
+    )
+}
 export default CalendarViewPage;
--- client/src/pages/ListViewPage.js
+++ client/src/pages/ListViewPage.js
@@ -1,5 +1,16 @@
-const ListViewPage = () =>(
-    <h1>ListViewPage</h1>
-);
+import Footer from "../component/Footer";
+import NavBar from "../component/NavBar";
+
+
+const ListViewPage = () =>{
+    return(
+        <>
+            <h1>List view</h1>
+            <NavBar/>
+            <Footer/>
+        </>
+        
+    )
+};
 
 export default ListViewPage;
