746a63d210c76152796f7a4a9ea2d0db8889c46d
Maya Sagalin <mayasag10@gmail.com>
2024-08-21 14:39:51 +0300

    added navbar shows only after correct login

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
+import './NavBar.css';
+import coolpanda from "../pics/coolpanda.jpeg";
+
 
 const NavBar = () => {
     return (
-        <nav>
+        <nav className='navbar'>
+            <img src={coolpanda} alt="panda"/>
             <ul>
                 <li>
                     <Link to="/ListView">ListView</Link>
+                </li>
+                <li>
                     <Link to="/CalendarView">CalendarView</Link>
                 </li>
             </ul>
--- client/src/pages/WelcomePage.js
+++ client/src/pages/WelcomePage.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../component/Footer";
 import coolpanda from "../pics/coolpanda.jpeg";
 import './WelcomePage.css';
+import NavBar from "../component/NavBar";
 
-const WelcomePage = () =>(
-    <div className="welcome-container">
-        <div className="header">
-            <div className="buttons">
-                <button className="button">Register</button>
-                <button className="button">Login</button>
+
+const WelcomePage = () =>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    //FIXME: for the real things
+    const handleLogin = () =>{
+        setIsLoggedIn(true);
+        alert("logged in");
+    }
+    const handleRegister = () =>{alert("registered!");} 
+
+    return(
+        <div className="welcome-container">
+            <div className="header">
+                <div className="buttons">
+                    <button className="button" onClick={handleRegister}>Register</button>  
+                    <button className="button" onClick={handleLogin}>Login</button>
+                </div>
+                <img src={coolpanda} alt="panda" className="pandaImage"/>
+                <h1>Welcome To My Task App</h1>
+                {isLoggedIn && <NavBar/>}
             </div>
-            <img src={coolpanda} alt="panda" className="pandaImage"/>
-            <h1>Welcome To My Task App</h1>
-            
+            <Footer/>
         </div>
-        <Footer/>
-    </div>
+    )
     
-);
-
+    
+};
+/**
+ * TODO: 
+ * 1. navbar only seen after successeful login 
+ * 2. action sheet on buttons 
+ */
 export default WelcomePage;
